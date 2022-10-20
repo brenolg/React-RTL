eee388893b6eaacebfc3cd0a002201564d3e7f4f
Breno Lavalle Garrido <brenolg@yahoo.com.br>
2022-10-20 19:35:44 -0300

    requisito 1?

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,16 @@
-test('', (sasa) => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+test('Teste se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
+  renderWithRouter(<App />);
+
+  const home = screen.getByRole('link ', { name: 'Home' });
+  const about = screen.getByRole('link ', { name: 'About' });
+  const fPok = screen.getByRole('link ', { name: 'Favorite Pokémons' });
+
+  expect(home).toBeInTheDocument();
+  expect(about).toBeInTheDocument();
+  expect(fPok).toBeInTheDocument();
+});
