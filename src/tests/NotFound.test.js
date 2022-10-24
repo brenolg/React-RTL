64aa3c14dfd25c40a628f55c9fcf48bf257315c1
Breno Lavalle Garrido <brenolg@yahoo.com.br>
2022-10-24 11:32:05 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,4 +1,14 @@
+import { screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
 describe('Teste o componente <FavoritePokemons.js />', () => {
-  test.todo('Teste se a página contém um heading h2 com o texto Page requested not found');
+  test('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    screen.logTestingPlaygroundURL();
+
+    const notFoundTitle = screen.getByRole('heading', { name: /page requested not found/i });
+    expect(notFoundTitle).toBeInTheDocument();
+  });
   test.todo('Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
 });
