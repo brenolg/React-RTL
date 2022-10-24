991337d4ef5368e74daa41d371133bfa7c7aa328
Breno Lavalle Garrido <brenolg@yahoo.com.br>
2022-10-24 11:22:56 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,19 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente FavoritePokemons', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha pokémons favoritos', () => {
+    renderWithRouter((<App />));
+
+    const favoriteLink = screen.getByRole('link', { name: /favorite pokémons/i });
+    userEvent.click(favoriteLink);
+    screen.logTestingPlaygroundURL();
+
+    const noFavPok = screen.getByText(/no favorite pokemon found/i);
+    expect(noFavPok).toBeInTheDocument();
+  });
+
+  test.todo('Teste se são exibidos todos os cards de pokémons favoritados');
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,4 @@
-test('', () => {});
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test.todo('Teste se a página contém um heading h2 com o texto Page requested not found');
+  test.todo('Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
